0335535f0f42b369eac3590ddb1dd080e52e7829
Jonas Pfefferle <pepperjo@japf.ch>
2015-07-17 13:03:29 +0200

    type traits -> aligned_type

---
+++ include/psl/type_traits.h
@@ -0,0 +1,33 @@
+#ifndef TYPE_TRAITS_H
+#define TYPE_TRAITS_H
+
+#include <utility>
+#include <cstddef>
+
+namespace psl {
+
+template<typename T, std::size_t Align>
+struct aligned_type
+{
+    private:
+        alignas(Align) T t;
+    public:
+        template<class... Args>
+        aligned_type(Args&&... args) : t(std::forward<Args>(args)...)
+        {
+        }
+
+        T* operator ->()
+        {
+            return &t;
+        }
+
+        T& operator*()
+        {
+            return t;
+        }
+};
+
+}
+
+#endif /* TYPE_TRAITS_H */
