474f070059df39b7a0f8fba64ba209adcf3147a4
Jonas Pfefferle <pepperjo@japf.ch>
2015-07-31 16:01:20 +0200

    use constepxr, fix correction function

--- include/psl/stats.h
+++ include/psl/stats.h
@@ -11,52 +11,78 @@ namespace psl {
 namespace stats {
 
 template <class InputIterator,
-          class Sum = plus_result_type<typename InputIterator::value_type>>
-double mean(InputIterator first, InputIterator last) {
+          class Sum = plus_result_type<
+              typename std::iterator_traits<InputIterator>::value_type>>
+constexpr double mean(InputIterator first, InputIterator last) {
     size_t count = 0;
     Sum sum = 0;
-    /* we could use std::accumulate here but if the iterator
-     * is not a RandomAccessIterator we would need to compute the size again
-     * (can be a O(n) operation, e.g. for lists) */
-    std::for_each(first, last, [&](decltype(*first)& e) {
+    /* 1. we could use std::accumulate here but if the iterator
+     * is not a RandomAccessIterator we need another O(n) to compute the size
+     * 2. we do not use std::for_each because of constexpr
+     */
+    for (; first != last; first++) {
         count++;
-        sum = sum + e;
-    });
+        sum = sum + *first;
+    }
 
     return sum / static_cast<double>(count);
 }
 
+constexpr size_t BesselCorrection(const size_t count) {
+    /* corrected/unbiased */
+    return count - 1;
+}
+
+constexpr size_t ShrinkageEstimator(const size_t count) {
+    /* Minimize mean squared error (biased) */
+    return count + 1;
+}
+
 template <class InputIterator,
-          class Sum = plus_result_type<typename InputIterator::value_type>>
-double variance(InputIterator first, InputIterator last, double mu) {
+          class Sum = plus_result_type<
+              typename std::iterator_traits<InputIterator>::value_type>,
+          class CorrectionFunction>
+constexpr double variance(InputIterator first, InputIterator last, double mu,
+                CorrectionFunction cf) {
     size_t count = 0;
     Sum sum = 0;
 
-    std::for_each(first, last, [&](decltype(*first)& e) {
+    for(; first != last; first++) {
         count++;
-        sum = sum + (e - mu) * (e - mu);
-    });
-    return sum / static_cast<double>(count - 1);
+        sum = sum + (*first - mu) * (*first - mu);
+    }
+
+    return sum / static_cast<double>(cf(count));
 }
 
 template <class InputIterator,
-          class Sum = plus_result_type<typename InputIterator::value_type>>
-double variance(InputIterator first, InputIterator last) {
-    return variance_m<InputIterator, Sum>(
-        first, last, mean<InputIterator, Sum>(first, last));
+          class Sum = plus_result_type<
+              typename std::iterator_traits<InputIterator>::value_type>,
+          class CorrectionFunction = decltype(BesselCorrection)>
+constexpr double variance(InputIterator first, InputIterator last,
+                CorrectionFunction cf = BesselCorrection) {
+    return variance<InputIterator, Sum, CorrectionFunction>(
+        first, last, mean<InputIterator, Sum>(first, last), cf);
 }
 
 template <class InputIterator,
-          class Sum = plus_result_type<typename InputIterator::value_type>>
-double stddev(InputIterator first, InputIterator last, double mu) {
-    return std::sqrt(variance_m<InputIterator, Sum>(first, last, mu));
+          class Sum = plus_result_type<
+              typename std::iterator_traits<InputIterator>::value_type>,
+          class CorrectionFunction>
+double stddev(InputIterator first, InputIterator last, double mu,
+              CorrectionFunction cf) {
+    return std::sqrt(
+        variance<InputIterator, Sum, CorrectionFunction>(first, last, mu, cf));
 }
 
 template <class InputIterator,
-          class Sum = plus_result_type<typename InputIterator::value_type>>
-double stddev(InputIterator first, InputIterator last) {
-    return stddev<InputIterator, Sum>(first, last,
-                                      mean<InputIterator, Sum>(first, last));
+          class Sum = plus_result_type<
+              typename std::iterator_traits<InputIterator>::value_type>,
+          class CorrectionFunction = decltype(BesselCorrection)>
+double stddev(InputIterator first, InputIterator last,
+              CorrectionFunction cf = BesselCorrection) {
+    return stddev<InputIterator, Sum, CorrectionFunction>(
+        first, last, mean<InputIterator, Sum>(first, last), cf);
 }
 
 template <class InputIterator>
